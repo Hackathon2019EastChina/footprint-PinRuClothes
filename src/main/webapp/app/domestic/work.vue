349b91f657c4ad18628a14e9818661f0dabf40bf
Chenshasan <48911319+Chenshasan@users.noreply.github.com>
2019-11-24 12:15:48 +0800

    007

--- src/main/webapp/app/domestic/work.vue
+++ src/main/webapp/app/domestic/work.vue
@@ -90,7 +90,7 @@
         </b-modal>
     </h2>  </b-col>
     </b-row>
-    <b-table striped hover :items="items" class="down"@row-clicked="art"></b-table>
+    <b-table striped hover :items="items" class="down" @row-clicked="art"></b-table>
      <footer id="footer" class="bg-white py-5 font-size-14">
     <div class="container">
         <div class="row">
